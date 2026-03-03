be936c3bd3fb0dd6a1ca2daab6481b83a8426e16
VictorAgahi <victor@agahi.com>
2026-03-03 09:59:52 +0100

    feat(controllers): update grpc method

--- src/modules/user/controllers/user.command.controller.ts
+++ src/modules/user/controllers/user.command.controller.ts
@@ -2,6 +2,7 @@ import { Controller } from '@nestjs/common';
 import { GrpcMethod } from '@nestjs/microservices';
 import {
   USER_SERVICE_NAME,
+  USER_GRPC_METHODS,
   type CreateUserCommand,
   type CreateUserResponse,
   type UpdateUserCommand,
@@ -12,17 +13,17 @@ import {
 
 @Controller()
 export class UserCommandController {
-  @GrpcMethod(USER_SERVICE_NAME, 'createUser')
+  @GrpcMethod(USER_SERVICE_NAME, USER_GRPC_METHODS.CREATE_USER)
   async createUser(_command: CreateUserCommand): Promise<CreateUserResponse> {
     throw new Error('Method not implemented.');
   }
 
-  @GrpcMethod(USER_SERVICE_NAME, 'updateUser')
+  @GrpcMethod(USER_SERVICE_NAME, USER_GRPC_METHODS.UPDATE_USER)
   async updateUser(_command: UpdateUserCommand): Promise<UpdateUserResponse> {
     throw new Error('Method not implemented.');
   }
 
-  @GrpcMethod(USER_SERVICE_NAME, 'deleteUser')
+  @GrpcMethod(USER_SERVICE_NAME, USER_GRPC_METHODS.DELETE_USER)
   async deleteUser(_command: DeleteUserCommand): Promise<DeleteUserResponse> {
     throw new Error('Method not implemented.');
   }
--- src/modules/user/controllers/user.query.controller.ts
+++ src/modules/user/controllers/user.query.controller.ts
@@ -2,6 +2,7 @@ import { Controller } from '@nestjs/common';
 import { GrpcMethod } from '@nestjs/microservices';
 import {
   USER_SERVICE_NAME,
+  USER_GRPC_METHODS,
   type UserQuery,
   type GetUserResponse,
   type ListUsersQuery,
@@ -10,12 +11,12 @@ import {
 
 @Controller()
 export class UserQueryController {
-  @GrpcMethod(USER_SERVICE_NAME, 'getUser')
+  @GrpcMethod(USER_SERVICE_NAME, USER_GRPC_METHODS.GET_USER)
   async getUser(_query: UserQuery): Promise<GetUserResponse> {
     throw new Error('Method not implemented.');
   }
 
-  @GrpcMethod(USER_SERVICE_NAME, 'listUsers')
+  @GrpcMethod(USER_SERVICE_NAME, USER_GRPC_METHODS.LIST_USERS)
   async listUsers(_query: ListUsersQuery): Promise<ListUsersResponse> {
     throw new Error('Method not implemented.');
   }
