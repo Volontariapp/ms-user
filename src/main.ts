ee76532a38f5f6a523d0bb115e0ad08c3210d599
VictorAgahi <victor@agahi.com>
2026-04-07 13:44:03 +0200

    feat(grpc): fix config

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,17 @@
 import './tracing.js';
 import 'reflect-metadata';
-import { dirname, join } from 'path';
-import { createRequire } from 'module';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { GRPC_SERVICES, getGrpcOptions } from '@volontariapp/contracts';
 import { AppConfigService } from './config/app-config.service.js';
-
-const require = createRequire(import.meta.url);
-
-const contractsPath = dirname(
-  require.resolve('@volontariapp/contracts/package.json'),
-);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(AppConfigService);
 
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.GRPC,
-    options: {
-      package: 'volontariapp.user',
-      protoPath: join(
-        contractsPath,
-        'proto/volontariapp/user/user.services.proto',
-      ),
-      url: configService.msUserUrl,
-      loader: {
-        keepCase: true,
-        longs: String,
-        enums: String,
-        defaults: true,
-        oneofs: true,
-        includeDirs: [join(contractsPath, 'proto')],
-      },
-    },
-  });
+  app.connectMicroservice(
+    getGrpcOptions(GRPC_SERVICES.USER, configService.msUserUrl),
+  );
 
   await app.startAllMicroservices();
   await app.listen(process.env.PORT ?? 3001);
