9428482d0a6b06c967c96ea026b6edfce4c586e4
VictorAgahi <victor@agahi.com>
2026-04-13 21:43:23 +0200

    features: health check + database registering

--- src/modules/user/controllers/user.command.controller.ts
+++ src/modules/user/controllers/user.command.controller.ts
@@ -1,8 +1,6 @@
 import { Controller } from '@nestjs/common';
 import { GrpcMethod } from '@nestjs/microservices';
 import {
-  USER_SERVICE_NAME,
-  USER_GRPC_METHODS,
   type CreateUserCommand,
   type CreateUserResponse,
   type UpdateUserCommand,
@@ -10,20 +8,21 @@ import {
   type DeleteUserCommand,
   type DeleteUserResponse,
 } from '@volontariapp/contracts';
+import { USER_SERVICE_NAME, USER_METHODS } from '@volontariapp/contracts-nest';
 
 @Controller()
 export class UserCommandController {
-  @GrpcMethod(USER_SERVICE_NAME, USER_GRPC_METHODS.CREATE_USER)
+  @GrpcMethod(USER_SERVICE_NAME, USER_METHODS.CREATE_USER)
   createUser(_command: CreateUserCommand): Promise<CreateUserResponse> {
     throw new Error('Method not implemented.');
   }
 
-  @GrpcMethod(USER_SERVICE_NAME, USER_GRPC_METHODS.UPDATE_USER)
+  @GrpcMethod(USER_SERVICE_NAME, USER_METHODS.UPDATE_USER)
   updateUser(_command: UpdateUserCommand): Promise<UpdateUserResponse> {
     throw new Error('Method not implemented.');
   }
 
-  @GrpcMethod(USER_SERVICE_NAME, USER_GRPC_METHODS.DELETE_USER)
+  @GrpcMethod(USER_SERVICE_NAME, USER_METHODS.DELETE_USER)
   deleteUser(_command: DeleteUserCommand): Promise<DeleteUserResponse> {
     throw new Error('Method not implemented.');
   }
--- src/providers/database/database.module.ts
+++ src/providers/database/database.module.ts
@@ -1,9 +1,42 @@
+import type { DynamicModule } from '@nestjs/common';
 import { Module } from '@nestjs/common';
-import { PostgresModule } from './postgres/postgres.module.js';
-import { Neo4jModule } from './neo4j/neo4j.module.js';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresBridgeModule } from '@volontariapp/bridge-nest';
+import type { PostgresConfig } from '@volontariapp/config';
 
-@Module({
-  imports: [PostgresModule, Neo4jModule],
-  exports: [PostgresModule, Neo4jModule],
-})
-export class DatabaseModule {}
+const entities: [] = [];
+
+@Module({})
+export class DatabaseModule {
+  static forRoot(dbConfig: PostgresConfig): DynamicModule {
+    return {
+      module: DatabaseModule,
+      imports: [
+        PostgresBridgeModule.register({
+          host: dbConfig.host,
+          port: dbConfig.port,
+          username: dbConfig.username,
+          password: dbConfig.password,
+          database: dbConfig.database,
+          ssl: dbConfig.ssl ? { rejectUnauthorized: false } : false,
+          entities,
+          synchronize: false,
+        }),
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          host: dbConfig.host,
+          port: dbConfig.port,
+          username: dbConfig.username,
+          password: dbConfig.password,
+          database: dbConfig.database,
+          ssl: dbConfig.ssl ? { rejectUnauthorized: false } : false,
+          entities,
+          synchronize: false, // Run migrations
+        }),
+        TypeOrmModule.forFeature(entities),
+      ],
+      exports: [TypeOrmModule],
+      global: true,
+    };
+  }
+}
