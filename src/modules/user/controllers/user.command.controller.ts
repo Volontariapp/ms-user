c2d1a07422154366fd15124e1e94d88169cc96b0
damien.lugagne-delpon <damien.lugagne-delpon@epita.fr>
2026-04-28 19:46:33 +0200

    feat(controller): base of controller

--- src/modules/user/controllers/user.command.controller.ts
+++ src/modules/user/controllers/user.command.controller.ts
@@ -1,29 +1,71 @@
 import { Controller } from '@nestjs/common';
 import { GrpcMethod } from '@nestjs/microservices';
 import {
-  type SignUpCommand,
-  type SignUpResponse,
-  type UpdateUserCommand,
-  type UpdateUserResponse,
-  type DeleteUserCommand,
-  type DeleteUserResponse,
-} from '@volontariapp/contracts';
-import { USER_SERVICE_NAME, USER_METHODS } from '@volontariapp/contracts-nest';
+  USER_SERVICE_NAME,
+  USER_COMMAND_METHODS,
+} from '@volontariapp/contracts-nest';
+import { Logger } from '@volontariapp/logger';
+import {
+  AuthService,
+  LoginInput,
+  UserId,
+  UserService,
+} from '@volontariapp/domain-user';
+import { UserTransformer } from '../transformers/user.transformer.js';
+import { SignUpCommandDTO } from '../dto/request/command/sign-up.command.dto.js';
+import { SignUpResponseDTO } from '../dto/response/sign-up.response.dto.js';
+import { LoginCommandDTO } from '../dto/request/command/login.command.dto.js';
+import { LoginResponseDTO } from '../dto/response/login.response.dto.js';
+import { UpdateUserCommandDTO } from '../dto/request/command/update-user.command.dto.js';
+import { UpdateUserResponseDTO } from '../dto/response/update-user.response.dto.js';
+import { DeleteUserCommandDTO } from '../dto/request/command/delete-user.command.dto.js';
 
 @Controller()
 export class UserCommandController {
-  @GrpcMethod(USER_SERVICE_NAME, USER_METHODS.CREATE_USER)
-  signUp(_command: SignUpCommand): Promise<SignUpResponse> {
-    throw new Error('Method not implemented.');
+  private readonly logger = new Logger({ context: UserCommandController.name });
+
+  constructor(
+    private readonly userService: UserService,
+    private readonly authService: AuthService,
+    private readonly userTransformer: UserTransformer,
+  ) {}
+
+  @GrpcMethod(USER_SERVICE_NAME, USER_COMMAND_METHODS.SIGN_UP)
+  async signUp(data: SignUpCommandDTO): Promise<SignUpResponseDTO> {
+    this.logger.log('gRPC: Signing up user with email: ' + data.email);
+    const signUpInput = this.userTransformer.toSignUpInput(data);
+    const response = await this.authService.signUp(signUpInput);
+    return {
+      user: this.userTransformer.toUserDTO(response.user),
+      auth: response.auth,
+    };
   }
 
-  @GrpcMethod(USER_SERVICE_NAME, USER_METHODS.UPDATE_USER)
-  updateUser(_command: UpdateUserCommand): Promise<UpdateUserResponse> {
-    throw new Error('Method not implemented.');
+  @GrpcMethod(USER_SERVICE_NAME, USER_COMMAND_METHODS.LOGIN)
+  async login(data: LoginCommandDTO): Promise<LoginResponseDTO> {
+    this.logger.log('gRPC: Logging in user with email: ' + data.email);
+    const tokens = await this.authService.logIn(
+      new LoginInput(data.email, data.password),
+    );
+    return { auth: tokens };
   }
 
-  @GrpcMethod(USER_SERVICE_NAME, USER_METHODS.DELETE_USER)
-  deleteUser(_command: DeleteUserCommand): Promise<DeleteUserResponse> {
-    throw new Error('Method not implemented.');
+  @GrpcMethod(USER_SERVICE_NAME, USER_COMMAND_METHODS.UPDATE_USER)
+  async updateUser(data: UpdateUserCommandDTO): Promise<UpdateUserResponseDTO> {
+    this.logger.log('gRPC: Updating user with id: ' + data.userId);
+    const partial = this.userTransformer.fromUserDTO(data);
+    const entity = await this.userService.update(
+      new UserId(data.userId),
+      partial,
+    );
+    return {
+      user: this.userTransformer.toUserDTO(entity),
+    };
+  }
+
+  @GrpcMethod(USER_SERVICE_NAME, USER_COMMAND_METHODS.DELETE_USER)
+  async deleteUser(data: DeleteUserCommandDTO): Promise<void> {
+    this.logger.log('gRPC: Deleting user with id: ' + data.userId);
+    await this.userService.delete(new UserId(data.userId));
   }
 }
--- src/modules/user/controllers/user.query.controller.ts
+++ src/modules/user/controllers/user.query.controller.ts
@@ -1,22 +1,57 @@
 import { Controller } from '@nestjs/common';
 import { GrpcMethod } from '@nestjs/microservices';
 import {
-  type GetUserQuery,
-  type UserResponse,
-  type ListUsersQuery,
-  type ListUsersResponse,
-} from '@volontariapp/contracts';
-import { USER_SERVICE_NAME, USER_METHODS } from '@volontariapp/contracts-nest';
+  USER_SERVICE_NAME,
+  USER_QUERY_METHODS,
+} from '@volontariapp/contracts-nest';
+import { GetUserQueryDTO } from '../dto/request/query/get-user.query.dto.js';
+import { UserResponseDTO } from '../dto/response/user.response.dto.js';
+import { Logger } from '@volontariapp/logger';
+import { UserTransformer } from '../transformers/user.transformer.js';
+import {
+  UserId,
+  UserService,
+  PaginationInput,
+} from '@volontariapp/domain-user';
+import { ListUsersQueryDTO } from '../dto/request/query/list-users.query.dto.js';
+import { ListUsersResponseDTO } from '../dto/response/list-users.response.dto.js';
 
 @Controller()
 export class UserQueryController {
-  @GrpcMethod(USER_SERVICE_NAME, USER_METHODS.GET_USER)
-  getUser(_query: GetUserQuery): Promise<UserResponse> {
-    throw new Error('Method not implemented.');
+  private readonly logger = new Logger({ context: UserQueryController.name });
+
+  constructor(
+    private readonly userService: UserService,
+    private readonly userTransformer: UserTransformer,
+  ) {}
+
+  @GrpcMethod(USER_SERVICE_NAME, USER_QUERY_METHODS.GET_USER)
+  async getUser(data: GetUserQueryDTO): Promise<UserResponseDTO> {
+    this.logger.log('gRPC: Getting user with id: ' + data.userId);
+    const user = await this.userService.findById(new UserId(data.userId));
+    return { user: this.userTransformer.toUserDTO(user) };
   }
 
-  @GrpcMethod(USER_SERVICE_NAME, USER_METHODS.LIST_USERS)
-  listUsers(_query: ListUsersQuery): Promise<ListUsersResponse> {
-    throw new Error('Method not implemented.');
+  @GrpcMethod(USER_SERVICE_NAME, USER_QUERY_METHODS.LIST_USERS)
+  async listUsers(data: ListUsersQueryDTO): Promise<ListUsersResponseDTO> {
+    this.logger.log('gRPC: Listing all users');
+    const pagination = data.pagination
+      ? new PaginationInput(
+          data.pagination.limit,
+          (data.pagination.page - 1) * data.pagination.limit,
+        )
+      : undefined;
+    const result = await this.userService.findAll(pagination);
+    const page = data.pagination?.page ?? 1;
+    const limit = data.pagination?.limit ?? result.total;
+    return {
+      users: result.users.map((user) => this.userTransformer.toUserDTO(user)),
+      pagination: {
+        total: result.total,
+        page,
+        limit,
+        totalPages: limit > 0 ? Math.ceil(result.total / limit) : 1,
+      },
+    };
   }
 }
