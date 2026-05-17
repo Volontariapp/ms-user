60dfa98379539a8980a56f2c7a629cf5a7bd9508
Clement Pasteau <100196557+Fyorix@users.noreply.github.com>
2026-05-17 23:02:42 +0200

    feat(health-check): added health-check to ms-user that was surprisingly missing :0 (#20)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { DynamicModule, Module } from '@nestjs/common';
 import { APP_FILTER, APP_PIPE } from '@nestjs/core';
 import { GlobalExceptionFilter } from '@volontariapp/errors-nest';
 import { GrpcValidationPipe } from '@volontariapp/validation-nest';
+import { HealthModule } from '@volontariapp/health-check-nest';
+import { TerminusModule } from '@nestjs/terminus';
 import { AppConfigModule } from './config/app-config.module.js';
 import type { CustomConfig } from './config/base-config.js';
 import { DatabaseModule } from './providers/database/database.module.js';
@@ -18,6 +20,11 @@ export class AppModule {
       imports: [
         AppConfigModule.forRoot(config),
         DatabaseModule.forRoot(config.db),
+        TerminusModule.forRoot({}),
+        HealthModule.register({
+          databases: ['postgres'],
+          failOnMissingProvider: true,
+        }),
         UserModule,
         GrpcClientModule,
       ],
